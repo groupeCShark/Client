f3d46c64b19f7bda6f2963ec25b3deddaa74c268
Jeremy Marin <jeremy.marin@student.ecp.fr>
2016-10-06 21:02:41 +0200

    Basic messaging interface

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,35 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Message> Messages = new ObservableCollection<Message>();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = Messages;
+
+            Messages.Add(new Message() { Username = "Server", Text = "Hello from the server" });
+            Messages.Add(new Message() { Username = "Me", Text = "Hi there!" });
+            Messages.Add(new Message() { Username = "Bob", Text = "Hey! Nice to see you again :-)" });
+        }
+
+        private void SendAction(object sender, RoutedEventArgs e)
+        {
+            Messages.Add(new Message() { Username = "Me", Text = TextInput.Text });
+            TextInput.Text = "";
+        }
+
+        public class Message
+        {
+            public string Username { get; set; }
+            public string Text { get; set; }
+            public string Display
+            {
+                get
+                {
+                    return Username + ":\n" + Text;
+                }
+            }
         }
     }
 }
---
+++ Client/MessageTemplateSelector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client
+{
+    class MessageTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate MyMessageTemplate { get; set; }
+        public DataTemplate OthersMessageTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item == null)
+                return null;
+
+            FrameworkElement frameworkElement = container as FrameworkElement;
+            Client.MainWindow.Message message = item as Client.MainWindow.Message;
+            if (frameworkElement != null)
+            {
+                bool myMessage = message.Username.Equals("Me");
+                if (myMessage)
+                {
+                    MyMessageTemplate = frameworkElement.FindResource("MyMessagesTemplate") as DataTemplate;
+                    return MyMessageTemplate;
+                }
+                else
+                {
+                    OthersMessageTemplate = frameworkElement.FindResource("OthersMessagesTemplate") as DataTemplate;
+                    return OthersMessageTemplate;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
